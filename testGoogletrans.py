6086189b8b7d64ba1992f20493d08a4e1a20f3dd
Ivan Ferre <ivan.ferre@powercoders.org>
2023-04-29 00:58:29 +0200

    Correct testGoogletrans.py

--- testGoogletrans.py
+++ testGoogletrans.py
@@ -17,10 +17,17 @@ translator = Translator()
 # If source language is not given, google translate attempts to detect the source language.
 #translator.translate('안녕하세요.')
 # <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
-translator.translate('Una bandera ens agermana!', dest='en', src='ca')
+flagSentence = translator.translate('Una bandera ens agermana!', dest='en', src='ca')
+print(flagSentence.text)
 
 # Array can be used to translate a batch of strings in a single method call and a single HTTP session. The exact same method shown above work for arrays as well.
-translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='ca')
-for translation in translations:
-    print(translation.origin, ' -> ', translation.text)
+#translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='ca')
+#for translation in translations:
+#    print(translation.origin, ' -> ', translation.text)
 
+english = ['The quick brown fox', 'jumps over', 'the lazy dog']
+for s in english:
+    ca = translator.translate(s, src='en', dest='ca')
+    print(f'{ca.origin} -> {ca.text}')
+
+print("That's all, folks!")
