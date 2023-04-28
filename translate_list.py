7744cfef783fb55ab286caf8a19f72fb44341a7a
Ivan Ferre <ivan.ferre@powercoders.org>
2023-04-29 00:46:01 +0200

    Add new comment on source of the original code.

--- translate_list.py
+++ translate_list.py
@@ -6,8 +6,9 @@ translator = Translator()
 
 data = ['Dobrý deň', 'majestátny orol', 'krehká dohoda']
 
+# Original example provided by https://zetcode.com/python/googletrans/
+# * It crashes, somehow.
 # translated = translator.translate(data, src='sk', dest='ca')
-
 # for trans in translated:
 #     print('Translating...')
 #     print(f'{trans.origin} -> {trans.text}')
