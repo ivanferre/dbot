fbd06c0e0e217a420c908ab57939f6beaeab1bba
Ivan Ferre <ivan.ferre@powercoders.org>
2023-05-01 16:28:48 +0200

    Debug comments on .

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+
+# FirstBot discord bot
+
+##### importing modules
+
 # for the discord bot
 import discord
 
@@ -11,28 +17,10 @@ from dotenv import load_dotenv
 import json
 import random   # encouragement
 
-# for the translation functionality
-import googletrans
-from googletrans import Translator
-translator = Translator(service_urls=[
- 	'translate.google.com',
-])
-translator = Translator()
-
-
-# Create the Discord bot.
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
+##### globals declaration
 
 # list of words that trigger bot's actions
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
-
-# we are performing a Stop
-ongoingStop = False
-ongoingStopUser = ''
 
 # answers to the triggers
 starter_encouragements = [
@@ -55,6 +43,29 @@ commands_list = [
     "$answer          (implementation in progress)"
 ]
 
+# we are performing a Stop
+ongoingStop = False
+ongoingStopUser = ''
+
+##### Initialization
+
+# for the translation functionality
+import googletrans
+from googletrans import Translator
+translator = Translator(service_urls=[
+ 	'translate.google.com',
+])
+translator = Translator()
+
+# Create the Discord bot.
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+##### functions
+
 # Get a quote from the Zenquotes API
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -62,10 +73,14 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
 
+#####
+##### Here we go!
+#####
+
 # Log the bot has logged in.
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('FirstBot logged in as {0.user}'.format(client))
 
 # Listener
 @client.event
@@ -78,6 +93,7 @@ async def on_message(message):
 # TODO: Replace message.author by user's name
     if message.content.startswith('$hello'):
         await message.channel.send(f'Hello, {message.author}!')
+        # print(f'Hello, {message.author}, nice to see you again.')
 
     if message.content.startswith('$inspire'):
         quote = get_quote()
@@ -85,30 +101,25 @@ async def on_message(message):
 
     if message.content.startswith('$help'):
         s = "These are the available commands:"
+        # ! FIX: the list of commands is not output
         await message.channel.send("These are the available commands:")
         while s in commands_list:
             await message.channel.send(s)
         else:
             await message.channel.send("Would you like to suggest further functionality?")
 
-    # translation functions
-    # msg == message.content
+    # translation functions (msg == message.content)
     if msg.startswith('$translate'):
         # remove '$translate ' from msg
         userText = msg.replace("$translate ", "")
         trad = translator.translate(userText,'en')
         await message.channel.send(f'{message.author} says: {trad.text}.')
-#        await message.channel.send(trad.text)
-        print(trad.text)
 
     if msg.startswith('$ubersetz '):
         # remove '$translate ' from msg
         userText = msg.replace("$ubersetz ", "")
         trad = translator.translate(userText,'de')
         await message.channel.send(f'{message.author} sagt: {trad.text}.')
-        # ! DEBUG
-        await message.channel.send(trad.text)
-        print(trad.text)
 
 
     # encouragement
@@ -116,17 +127,31 @@ async def on_message(message):
         await message.channel.send(random.choice(starter_encouragements))
 
     if message.content.startswith('$stopinstance'):
+        global ongoingStop
+        global ongoingStopUser
         ongoingStop = True;
         ongoingStopUser = message.author;
-        await message.channel.send('Are you sure you want to STOP the instance, {message.author}?')
+        await message.channel.send(f'Are you sure you want to STOP the instance, {message.author}?')
 
 # TODO implement actual stop the AWS instance.
 # ! FIX: why can't find the definition of the ongoing variables?
     if message.content.startswith('$confirmstop'):
-        if (ongoingStop & ({message.author} == ongoingStopUser)):
-            await message.channel.send('Goodbye, {message.author} and everybody else!')
+        #global ongoingStop
+        #global ongoingStopUser
+        # ! DEBUG
+        print (f'ongoingStop = {ongoingStop}')
+        print (f'ongoingStopUser = {ongoingStopUser}')
+        print (f'message.author = {message.author}')
+        print (f'(message.author == ongoingStopUser) = {({message.author} == ongoingStopUser)}')
+        if (str1 == str2):
+            print ("Both strings are equal")
         else:
-            await message.channel.send('{message.author}, you should not make fun with such important matters.')
+            print ("Strings are different.")
+
+        if (ongoingStop & ({message.author} == ongoingStopUser)):
+            await message.channel.send(f'Goodbye, {message.author} and everybody else!')
+        else:
+            await message.channel.send(f'{message.author}, you should not make fun with such important matters.')
 
 btoken = os.getenv('TOKEN')
 client.run(btoken)
