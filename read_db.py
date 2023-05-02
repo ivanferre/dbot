0aaa72cf3cd40f3ff8d2ac071b7452f2eaf1a621
Ivan Ferre <ivan.ferre@powercoders.org>
2023-05-02 18:38:04 +0200

    Changed message.author to message.author.name in a couple of places.

---
+++ read_db.py
@@ -0,0 +1,23 @@
+# read_db.py
+
+# connect to db
+
+# read table SAD_EXPRESSIONS and
+# populate list sad_words
+
+# read table ENCOURAGEMENTS and
+# populate table starter_encouragements
+
+# insert new question
+
+# update question with temptative answer
+# pending to accept
+
+# update question with acceptance to answer
+
+# insert meeting
+
+# update meeting
+
+# delete meeting
+
