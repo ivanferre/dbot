ab04d4a78ac1c560a577e47dd15417bbcbcf9ce5
Ivan Ferre <ivan.ferre@powercoders.org>
2023-05-17 18:04:24 +0200

    Change default values at globals.

--- globals.py
+++ globals.py
@@ -7,8 +7,8 @@ strTimeFormat = '%d.%m.%y %H:%M:%S'
 
 # default minutes between reminders to answer questions
 # 60 x 24  = 1440
-reminderPeriod = 1440
+reminderPeriod = 1
 
 # default minutes to wait for answers a question
 # 1440 x 7 = 10080
-answersDeadline = 10080
+answersDeadline = 3
